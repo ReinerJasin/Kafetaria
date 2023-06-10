d49687b6d6b642eb862d70c23edfb4532a8edaf4
ReinerJasin <62686723+ReinerJasin@users.noreply.github.com>
2023-06-10 14:33:30 +0700

    Add route to Cart  to see cart list

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MenuController::class, 'index']);
 Route::get('/history', [CartController::class, 'history']);
-// Route::get('/', [MenuController::class, 'index']);
+Route::get('/cart', [CartController::class, 'cart']);
